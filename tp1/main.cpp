89cf105b72e12d8d54bfbb3a0f25562ee80fcf9a
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-11-10 22:42:01 -0300

    parsing input

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -9,7 +9,7 @@ std::vector<std::vector<GroundType>>
 parse_input_file(const std::string &filename) {
   std::ifstream input_stream(filename);
   int height, width;
-  input_stream >> height >> width;
+  input_stream >> width >> height;
   std::vector<std::vector<GroundType>> map_(height,
                                             std::vector<GroundType>(width));
   for (int i = 0; i < height; i++) {
@@ -17,8 +17,20 @@ parse_input_file(const std::string &filename) {
       char c;
       input_stream >> c;
       switch (c) {
-      case '@':
+      case '.':
+        map_.at(i).at(j) = GroundType::Grass;
+        break;
+      case ';':
+        map_.at(i).at(j) = GroundType::HighGrass;
+        break;
+      case '+':
+        map_.at(i).at(j) = GroundType::Water;
+        break;
+      case 'x':
         map_.at(i).at(j) = GroundType::Fire;
+        break;
+      case '@':
+        map_.at(i).at(j) = GroundType::Wall;
         break;
       default:
         std::cerr << "Unkown ground type in input file" << std::endl;
@@ -29,4 +41,17 @@ parse_input_file(const std::string &filename) {
   return map_;
 }
 
-int main(int argc, char *argv[]) { exit(EXIT_SUCCESS); }
+int main(int argc, char *argv[]) {
+  if (argc != 7) {
+    std::cerr << "Usage: " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  std::vector<std::vector<GroundType>> map_ = parse_input_file(argv[1]);
+  for (auto e1 : map_) {
+    for (auto e2 : e1) {
+      std::cout << e2 << " ";
+    }
+    std::cout << std::endl;
+  }
+  exit(EXIT_SUCCESS);
+}
