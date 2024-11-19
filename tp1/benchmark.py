0a33f4069432869ac1f610e60de11d0eb95fe838
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-11-18 23:21:56 -0300

    adiantando report

--- tp1/benchmark.py
+++ tp1/benchmark.py
@@ -4,26 +4,33 @@ import random
 import sys
 
 ALGORITHMS: list[str] = ["BFS", "IDS", "UCS", "Greedy", "Astar"]
-NUMBER_OF_RUNS: int = 1
+NUMBER_OF_RUNS: int = 10
 
 
 def parse_input_and_retrieve_non_wall_coordinates(
-    inputfile: str,
-) -> list[tuple[int, int]]:
+    input_file: str,
+) -> tuple[list[tuple[int, int]], int, int]:
     non_wall_coordinates: list[tuple[int, int]] = []
-    return non_wall_coordinates
+    H, W = None, None
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+        H, W = tuple(map(lambda x: int(x), lines[0].split(" ")))
+        for i, line in enumerate(lines[1:]):
+            i += 1
+            for j, ground in enumerate(line):
+                j += 1
+                if ground != "@":
+                    non_wall_coordinates.append((i, j))
+
+    return non_wall_coordinates, H, W
 
 
 input_file: str = sys.argv[1]
-parse_input_and_retrieve_non_wall_coordinates(input_file)
+non_wall_coordinates, H, W = parse_input_and_retrieve_non_wall_coordinates(input_file)
 
-H, W = 256, 256  # hardcoded do cidades.map
 for _ in range(NUMBER_OF_RUNS):
-    x_i: int = random.randint(1, 256)
-    x_f: int = random.randint(1, 256)
-    y_i: int = random.randint(1, 256)
-    y_f: int = random.randint(1, 256)
-    print(x_i, x_f, y_i, y_f)
+    x_i, y_i = random.choice(non_wall_coordinates)
+    x_f, y_f = random.choice(non_wall_coordinates)
     for algorithm in ALGORITHMS:
         start: float = time.perf_counter()
         output: str = subprocess.run(
@@ -44,3 +51,4 @@ for _ in range(NUMBER_OF_RUNS):
         print(algorithm, end=" ")
         print(cost, end=" ")
         print(total_time)
+    print("--------------------------------------")
